78ef343ea7c7313b9c2910ac921462f017d32821
chenbo <17673488086@126.com>
2023-06-07 12:29:54 +0800

    #fix commit

--- transit-program/src/main/java/com/example/transitprogram/controller/TransitController.java
+++ transit-program/src/main/java/com/example/transitprogram/controller/TransitController.java
@@ -6,6 +6,7 @@ import com.example.transitprogram.service.RkrltService;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,16 +21,21 @@ public class TransitController {
 
     @PostMapping("/rkrlt")
     public JSONObject transitRkrlt(@RequestBody RkrltDTO rkrltDTO){
-        System.out.println(rkrltDTO);
-        System.out.println("hello");
-        Assert.isTrue(null!=rkrltDTO,"body参数不能为空！");
         String baseUrl = rkrltDTO.getBaseUrl();
         String appKey = rkrltDTO.getAppKey();
         String method = rkrltDTO.getMethod();
         String service = rkrltDTO.getService();
         String appSercert = rkrltDTO.getAppSercert();
         HashMap<String, String> query = rkrltDTO.getQuery();
-        Assert.isTrue(!baseUrl.isEmpty()&&!appKey.isEmpty()&&!method.isEmpty()&&!service.isEmpty()&&!appSercert.isEmpty(),"缺少必要参数！");
-        return rkrltService.transitRkrlt(baseUrl,appKey,appSercert,service,method,query);
+
+        Assert.isTrue((!StringUtils.isEmpty(baseUrl)&&!StringUtils.isEmpty(appKey)&&!StringUtils.isEmpty(method)&&!StringUtils.isEmpty(service)&&!StringUtils.isEmpty(appSercert)),"缺少必要参数！");
+
+        try {
+            JSONObject jsonObject = rkrltService.transitRkrlt(baseUrl, appKey, appSercert, service, method, query);
+            return jsonObject;
+        }catch (Exception e){
+            System.out.println("exception");
+            return JSONObject.parseObject(e.getMessage());
+        }
     }
 }
