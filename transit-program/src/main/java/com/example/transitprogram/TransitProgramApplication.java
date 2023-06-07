c55b7098320abe8ed1ca697e44a0a4bec91e2360
chenbo <17673488086@126.com>
2023-06-07 11:14:15 +0800

    #second commit

---
+++ transit-program/src/main/java/com/example/transitprogram/TransitProgramApplication.java
@@ -0,0 +1,13 @@
+package com.example.transitprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransitProgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransitProgramApplication.class, args);
+    }
+
+}
---
+++ transit-program/src/test/java/com/example/transitprogram/TransitProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transitprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransitProgramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
